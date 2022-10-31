67fbedc68358aa944686c5675b34ce64b3fed112
tdeng2021 <dt_ecust@126.com>
2022-10-31 17:51:33 -0500

    json-server,Axios,
    RequestProvider, useResource hook in order to retrieve Todos. Completed lecture 5. And StateContext has been updated in the last commit.

---
+++ Lab4_summit/frontend/src/App.js
@@ -0,0 +1,48 @@
+import { useState, useEffect, useReducer } from "react";
+import CreatePost from "./Post/CreateTodo";
+import PostList from "./Post/TodoList";
+import UserBar from "./user/UserBar";
+import appReducer from "./reducer";
+import { StateContext } from "./contexts";
+import { useResource } from "react-request-hook";
+
+function App() {
+  const initalTodos = [];
+  //const [user, setUser] = useState('');
+
+  const [state, dispatch] = useReducer(appReducer, {
+    user: "",
+    todos: initalTodos,
+  });
+
+  //const [todos, setTodos] = useState(initalTodos);
+
+  document.title = "Todo";
+
+  const [todos, getTodos] = useResource(() => ({
+    url: "/todos",
+    method: "get",
+  }));
+
+  useEffect(getTodos, []);
+
+  useEffect(() => {
+    if (todos && todos.data) {
+      dispatch({ type: "FETCH_POSTS", todos: todos.data.reverse() });
+    }
+  }, [todos]);
+
+  return (
+    <div>
+      <StateContext.Provider value={{ state, dispatch }}>
+        <h1>Todo List</h1>
+
+        <UserBar />
+        <PostList />
+        {state.user && <CreatePost />}
+      </StateContext.Provider>
+    </div>
+  );
+}
+
+export default App;
